17b67065f42148b875862d97a38e3c8f3049c577
csparkzxc <csparkzxc1@gmail.com>
2026-04-24 01:02:28 +0900

    fix(mobile): metro.config.js에서 unstable_enablePackageExports 제거
    - unstable_enableSymlinks는 유지 (pnpm 심볼릭 링크 필수)
    
    - unstable_enablePackageExports 제거 (NativeWind와 충돌)
    
    빌드 에러: Unable to resolve react-native-css-interop/jsx-runtime
    
    원인: package.json exports 필드 해석이 css-interop 서브경로를 깨뜨림
    
    이전 빌드 실패: f683efa3-2df5-4ce2-aea3-2ef02e299b45
    
    참고: 2차 빌드에서 추가했으나 3차 빌드 실패 원인으로 판명.

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -12,6 +12,5 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, "node_modules"),
 ];
 config.resolver.unstable_enableSymlinks = true;
-config.resolver.unstable_enablePackageExports = true;
 
 module.exports = withNativeWind(config, { input: "./global.css" });
